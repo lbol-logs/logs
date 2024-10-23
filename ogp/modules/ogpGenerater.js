f70953b3bf5de5abee9a19f0a751425d389c6e3e
imat-cww <imat.cww@gmail.com>
2024-10-23 14:28:31 +0900

    add `await` to `sharp`

--- ogp/modules/ogpGenerater.js
+++ ogp/modules/ogpGenerater.js
@@ -2,40 +2,41 @@ import fs from 'fs';
 import sharp from 'sharp';
 import TextToSVG from 'text-to-svg';
 
-const generateOGP = function () {
+const generateOGP = async function () {
   // TODO: check ogp existance
   // TODO: locales
- const textToSVG = TextToSVG.loadSync('./static/assets/fonts/NotoSerifJP-Medium.otf');
- const fileNames = fs.readdirSync('./static/assets/data');
- for (const key in fileNames) {
-  const file = JSON.parse(fs.readFileSync('./static/assets/data/' + fileNames[key], 'utf8'));
-  const textSvg = textToSVG.getSVG(file.title, {
-   x: 0,
-   y: 0,
-   fontSize: 100,
-   anchor: 'top',
-   attributes: { fill: 'black', stroke: 'white' }
-  })
-  sharp('./static/assets/images/' + file.thumbnail)
-   .composite([
-    {
-     input: Buffer.from(textSvg)
-    }
-   ])
-   .resize(1200, 630)
-   .toFile('./static/ogp/' + file.slug + '.png', (error) => {
-    // eslint-disable-next-line no-console
-    if (error) console.log('OGP Generate Error: ' + error)
-   })
- }
+  const textToSVG = TextToSVG.loadSync('./static/assets/fonts/NotoSerifJP-Medium.otf');
+  const fileNames = fs.readdirSync('./static/assets/data');
+  console.log({ fileNames });
+  for (const key in fileNames) {
+    const file = JSON.parse(fs.readFileSync('./static/assets/data/' + fileNames[key], 'utf8'));
+    const textSvg = textToSVG.getSVG(file.title, {
+      x: 0,
+      y: 0,
+      fontSize: 100,
+      anchor: 'top',
+      attributes: { fill: 'black', stroke: 'white' }
+    });
+    await sharp('./static/assets/images/' + file.thumbnail)
+      .composite([
+        {
+          input: Buffer.from(textSvg)
+        }
+      ])
+      .resize(1200, 630)
+      .toFile('./static/ogp/' + file.slug + '.png', (error) => {
+        // eslint-disable-next-line no-console
+        if (error) console.log('OGP Generate Error: ' + error);
+      });
+  }
 };
 
 module.exports = function () {
- this.nuxt.hook('generate:before', (generator) => {
-  // eslint-disable-next-line no-console
-  console.log('OgpGenerater:start')
-  generateOGP()
-  // eslint-disable-next-line no-console
-  console.log('OgpGenerater:finish')
- })
+  this.nuxt.hook('generate:before', (generator) => {
+    // eslint-disable-next-line no-console
+    console.log('OgpGenerater:start')
+    generateOGP();
+    // eslint-disable-next-line no-console
+    console.log('OgpGenerater:finish')
+  })
 };
